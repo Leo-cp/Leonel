c0d9cca2a2408cabb1bd894f10388e1e23f1eb1e
Nyuykongi Leonel <nyuykongileonel36@gmail.com>
2025-07-23 14:42:53 +0100

    i have completed variables in python

---
+++ 2.0 software 1.0/1.0 Python basics/Day 1/Variables.py/variables.py
@@ -0,0 +1,38 @@
+# # # print("Hello DeepSeeds")
+
+# # # #why do we need variables
+# # # name= "Geo"
+# # # print("I am Leo, i love this name Leo, was given to me by my father, paul Leo, and i really love the name, Leo")
+# # # print("My name is "  + name +  "i love "  + name +  " was given by my father, "  + name +  " paul")
+# # # #strings and codes
+# # # whole_number=42
+# # # decimal_number=3.14159
+# # # complex_number=2+3j
+# # # # text
+# # # greeting ="Hello,world!"
+# # # single_char = 'A'
+# # # is_sunny=True
+# # # is_rainy=False
+# # # # check the type of variable
+# # # print(type(whole_number))
+# # # print(type(greeting))
+# # # first_name="Leonel"
+# # # last_name="Nyuykongi"
+# # # full_name=first_name +" " + last_name
+# # # print(My fullnames are )
+# # # a ban name generator
+# # name=input("What is your name?")
+# # age=input("How old are you?")
+# # favorite_meal=input("What is your favorite meal?")
+# # school=input("where do you school?")
+# # department=input("which department?")
+# # best_friend=input("what is the name of your best friend?")
+# # print("My name is "+ name +" I am "+age + "years old. my favorite meal is "+favorite_meal + ". I school in "+school + " in the department of " +department +". I dont have a best friend." )
+# lenth=10
+# width=5
+# Area=lenth*width
+# print("Your Area is:", Area )
+temp_in_celsius=input("what is your temperature")
+converted_temp=float(temp_in_celsius)
+results=(converted_temp*9/5)+32
+print(f"My results: {results}")
