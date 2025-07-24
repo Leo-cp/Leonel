530fa7e802cfc00e6e308be7ecf6d0129d902a24
Nyuykongi Leonel <nyuykongileonel36@gmail.com>
2025-07-24 10:08:51 +0100

    i am done with string

---
+++ 2.0 software 1.0/1.0 Python basics/Day 1/Variables.py/2. strings.py
@@ -0,0 +1,58 @@
+# #  # # message="Hello, python!"
+# # # # print(len(message))
+# # # name="Alice"
+# # # age=30
+# # # score=95.5
+# # # print(f"Hello, {name}! You are {age} years old.")
+# # # print(f"your score is {score:.1f}%")
+# # # #method 2: format () method
+# # # print("hello, {}! you are {} years old." .format(name,age))
+# # # #method 3: %formatting (older style)
+# # # print("hi, %s! you are %d years old." %(name,age))
+# # email="user@example.com"
+# # if "@" in email and "." in email: 
+# #     username= email.split("@")[0] 
+# #     domain= email.split("@")[1] 
+# #     print(f"username: {username}")
+# #     print(f"domain: {domain}")
+# # else:
+# #     print("Invalid email format")
+# #24th july 
+# message="good morning"
+# message_lower=message.lower()
+
+full_name="johN dOe"
+j=full_name[0].upper()
+n=full_name[3].lower()
+d=full_name[5].upper()
+o=full_name[6].lower()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
